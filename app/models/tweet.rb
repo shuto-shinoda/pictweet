acac37435293b16a2f0fe1e737602578fa4f2507
shuto-shinoda <shuto.88.tb@gmail.com>
2020-10-31 23:05:06 +0900

    first commit & エラー表示

---
+++ app/models/tweet.rb
@@ -0,0 +1,18 @@
+class Tweet < ApplicationRecord
+  validates :text, presence: true
+  belongs_to :user
+  has_many :comments
+
+  # imageも空で投稿できないように追記
+  validates :text, :image, presence: true
+
+  def self.search(search)
+    if search != ""
+      Tweet.where('text LIKE(?)', "%#{search}%")
+    else
+      Tweet.all
+    end
+    search = "%#{search}%"
+    Tweet.find_by_sql(["select * from tweets where text like ? ", search])
+  end
+end
